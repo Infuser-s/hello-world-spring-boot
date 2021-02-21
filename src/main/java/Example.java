e43c20c012bd52d598d889af4868aefc29dcd8bd
Prasad Bayi <74462843+prasadbayi@users.noreply.github.com>
2021-02-21 09:05:45 +0530

    introduced Time now.

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -1,7 +1,9 @@
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
-import org.springframework.web.bind.annotation.*;
+import java.time.LocalDateTime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @EnableAutoConfiguration
@@ -9,7 +11,7 @@ public class Example {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World - Springboot - TESTING CI-CD - By BAYI @ 12:13 AM 21 Feb 2021 IST";
+        return "Hello World - Springboot - TESTING CI-CD - By BAYI @ 12:13 AM 21 Feb 2021 IST. API served at: " + LocalDateTime.now();
     }
 
     public static void main(String[] args) throws Exception {
